6ee2be3fcc11478ee6306003fbdce51f68657b20
Brandon Conway <brandoncc@gmail.com>
2021-11-22 23:07:50 -0800

    Update marks to use new harpoon API

--- lua/telescope/_extensions/harpoon/marks.lua
+++ lua/telescope/_extensions/harpoon/marks.lua
@@ -7,6 +7,7 @@ local finders = require('telescope.finders')
 local pickers = require('telescope.pickers')
 local previewers = require('telescope.previewers')
 local sorters = require('telescope.sorters')
+local harpoon_mark = require('harpoon.mark')
 
 local default_options = {
   results_title = 'Harpoon Marks',
@@ -14,9 +15,27 @@ local default_options = {
   previewer = previewers.vim_buffer_cat.new {}
 }
 
+local function mark_is_empty(mark)
+  return mark.filename == "(empty)" or mark.filename == ""
+end
+
 return function(opts)
   opts = opts or {}
-  opts.finder = finders.new_table(harpoon.get_mark_config().marks)
 
+  local config = harpoon.get_mark_config()
+  local file_list = config.marks
+  local items = {}
+
+  for idx = 1, #file_list do
+    local mark = harpoon_mark.get_marked_file(idx)
+    if mark and not mark_is_empty(mark) then
+      table.insert(
+        items,
+        string.format("%s:%d:%d", mark.filename, mark.row, mark.col)
+      )
+    end
+  end
+
+  opts.finder = finders.new_table(items)
   pickers.new(opts, default_options):find()
 end
